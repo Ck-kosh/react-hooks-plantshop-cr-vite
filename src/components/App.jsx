fbb8d54aa05409966d00267ef0fe2e05527fa101
Ck-kosh <elias.cheruiyot@student.moringaschool.com>
2026-05-12 13:57:46 +0300

    fist commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+ 
+  function handleAddPlant(newPlant) {  // Add new plant to state
+    setPlants([...plants, newPlant]);
+  }
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+
+      <PlantPage
+        plants={filteredPlants}
+        onAddPlant={handleAddPlant}
+        search={search}
+        setSearch={setSearch}
+      />
     </div>
   );
 }
